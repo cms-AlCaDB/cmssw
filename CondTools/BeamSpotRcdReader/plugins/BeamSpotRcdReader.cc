36dbbf53cbfdb844b5614dc44fc70b40be450f31
mmusich <marco.musich@cern.ch>
2016-10-18 14:12:10 +0200

    Add utility to plot Beamspot

---
+++ CondTools/BeamSpotRcdReader/plugins/BeamSpotRcdReader.cc
@@ -0,0 +1,144 @@
+// -*- C++ -*-
+//
+// Package:    CondTools/BeamSpotRcdReader
+// Class:      BeamSpotRcdReader
+// 
+/**\class BeamSpotRcdReader BeamSpotRcdReader.cc CondTools/BeamSpotRcdReader/plugins/BeamSpotRcdReader.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Marco Musich
+//         Created:  Tue, 18 Oct 2016 11:00:44 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h"
+#include "CondFormats/BeamSpotObjects/interface/BeamSpotObjects.h"
+
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class BeamSpotRcdReader : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit BeamSpotRcdReader(const edm::ParameterSet&);
+      ~BeamSpotRcdReader();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&);
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      edm::ESWatcher<BeamSpotObjectsRcd> watcher_;
+      edm::LuminosityBlockNumber_t firstLumi_;
+      edm::LuminosityBlockNumber_t lastLumi_;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+BeamSpotRcdReader::BeamSpotRcdReader(const edm::ParameterSet& iConfig)
+
+{
+   //now do what ever initialization is needed
+   usesResource("TFileService");
+
+}
+
+
+BeamSpotRcdReader::~BeamSpotRcdReader()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+BeamSpotRcdReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+   
+   if (watcher_.check(iSetup)) { // new IOV for this run
+     
+     std::cout << " for runs: " << iEvent.id().run() << " - " << iEvent.id().luminosityBlock() << std::endl;
+     // Get BeamSpot from EventSetup:
+     edm::ESHandle< BeamSpotObjects > beamhandle;
+     iSetup.get<BeamSpotObjectsRcd>().get(beamhandle);
+     const BeamSpotObjects *mybeamspot = beamhandle.product();
+     std::cout << *mybeamspot << std::endl;
+   }
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+BeamSpotRcdReader::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+BeamSpotRcdReader::endJob() 
+{
+}
+
+void 
+BeamSpotRcdReader::endLuminosityBlock(const edm::LuminosityBlock& lb, const edm::EventSetup& es){}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+BeamSpotRcdReader::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(BeamSpotRcdReader);
---
+++ CondTools/BeamSpotRcdReader/test/BeamSpotRcdRead_cfg.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("READ")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000000) )
+
+process.source = cms.Source("EmptySource",
+                            numberEventsInRun = cms.untracked.uint32(5000),
+                            firstRun = cms.untracked.uint32(273291),
+                            numberEventsInLuminosityBlock = cms.untracked.uint32(1),
+                            firstLuminosityBlock = cms.untracked.uint32(1)
+                            )
+
+# either from Global Tag
+# process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi")
+# from Configuration.AlCa.GlobalTag import GlobalTag
+# process.GlobalTag = GlobalTag(process.GlobalTag,"auto:run2_data")
+
+# ...or specify database and tag:  
+from CondCore.CondDB.CondDB_cfi import *
+CondDBBeamSpotObjects = CondDB.clone(connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'))
+process.dbInput = cms.ESSource("PoolDBESSource",
+                              CondDBBeamSpotObjects,
+                              toGet = cms.VPSet(cms.PSet(record = cms.string('BeamSpotObjectsRcd'),
+                                                         tag = cms.string('BeamSpotObjects_2016B_v2_LumiBased_TEST_offline') # choose tag you want
+                                                         )
+                                                )
+                              )
+
+process.beamspot = cms.EDAnalyzer("BeamSpotRcdReader")
+
+# Put module in path:
+process.p = cms.Path(process.beamspot)
